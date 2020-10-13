1ce0910b502f3c5b2684a15797da85ec33730b1c
Shreyans Jain <jainshreyans121@gmail.com>
2020-10-13 11:53:36 +0530

    added modal

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function vowelsCounter() {
+
+  let modal = document.getElementById("myModal");
   
   let text = document.getElementById("ip").value;
   let check = text.match(/[aeiou]/gi);
@@ -8,5 +10,12 @@ function vowelsCounter() {
       count = check.length;
   } 
 
+  modal.style.display = "block";
+
   document.getElementById("ans").innerHTML = "Count(Vowels) : " + count;
+}
+
+function exit() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
 }
